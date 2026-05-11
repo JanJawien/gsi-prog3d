24ceecb19959c0fd007f10166f6e783f5e3bc98a
unknown <jawien.janek@gmail.com>
2026-05-12 00:27:49 +0200

    main light dimmed

--- src/LightHandler.cpp
+++ src/LightHandler.cpp
@@ -34,7 +34,7 @@ void LightHandler::SceneLightDistribute(int targetCount){
 void LightHandler::InitLights() {
     // Ambient light
     lightsOther.push_back({
-        { -3.0f, 4.5f, 0.0f }, 5.0f,
+        { -3.0f, 4.5f, 0.0f }, 3.0f,
         { 1.0f, 0.8f, 0.6f }, 20.0f,
         { 0,0,0 }, 1.0f,
         0, 1, {}
