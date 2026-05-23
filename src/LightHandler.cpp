9d3e3cff3b47ab5878cf4210f3561f7dcd237809
unknown <jawien.janek@gmail.com>
2026-05-23 16:01:56 +0200

    more lighting effects

--- src/LightHandler.cpp
+++ src/LightHandler.cpp
@@ -62,6 +62,18 @@ void AnglesToRotVector(XMFLOAT3* destVec, float angleVertical, float angleHorizo
         tanf(angleHorizontal * 3.14159265f / 180.0f), 0.0f)));
 }
 
+XMFLOAT3 FloatToHue(float t) {
+    float r = fabs(t * 6.0f - 3.0f) - 1.0f;
+    float g = 2.0f - fabs(t * 6.0f - 2.0f);
+    float b = 2.0f - fabs(t * 6.0f - 4.0f);
+
+    return XMFLOAT3(
+        std::clamp(r, 0.0f, 1.0f),
+        std::clamp(g, 0.0f, 1.0f),
+        std::clamp(b, 0.0f, 1.0f)
+    );
+}
+
 
 
 // ----- getters -----
@@ -197,22 +209,12 @@ void LightHandler::ChangeLightEffect(int effectIndex) {
         lightEffectPeriod = 16;
         angVFunc = [](float z, float t, int i) { return 60.0f * (-cos((t + abs(z / 10)) * XM_2PI) / 2 + 0.5f); };
         angHFunc = [](float z, float t, int i) { return 45.0f * z / 3;};
-        colorFunc = [](float z, float t, int i) {
-                float r = fabs(t * 6.0f - 3.0f) - 1.0f;
-                float g = 2.0f - fabs(t * 6.0f - 2.0f);
-                float b = 2.0f - fabs(t * 6.0f - 4.0f);
-
-                return XMFLOAT3(
-                    std::clamp(r, 0.0f, 1.0f),
-                    std::clamp(g, 0.0f, 1.0f),
-                    std::clamp(b, 0.0f, 1.0f)
-                );
-            };
+        colorFunc = [](float z, float t, int i) { return FloatToHue(t); };
         break;
 
     case 2: // White flashing, 45deg to front and slight apart
         for (auto& l : lightsScene) {
-            l.color = { 1.0f, 0.0f, 0.0f };
+            l.color = { 1.0f, 1.0f, 1.0f };
             l.direction = { -1.0f, -1.0f, l.position.z / 24 };
             l.isEnabled = true;
         }
@@ -221,6 +223,45 @@ void LightHandler::ChangeLightEffect(int effectIndex) {
         angHFunc = NULL;
         colorFunc = [](float z, float t, int i) {
             if((t>0.5) ^ (i%2==0)) return XMFLOAT3{ 1.0f, 1.0f, 1.0f };
+            else return XMFLOAT3{ 0.0f, 0.0f, 0.0f };
+            };
+        break;
+
+    case 3: // Different colors spinning in 8 shape
+        for (auto& l : lightsScene) {
+            l.color = FloatToHue(l.position.z/6 + 0.5f);
+            l.direction = { -1.0f, -1.0f, 0.0f };
+            l.isEnabled = true;
+        }
+        lightEffectPeriod = 16;
+        angVFunc = [](float z, float t, int i) { return 30.0f + 15.0f * cos(2*t * XM_2PI + z*8); };
+        angHFunc = [](float z, float t, int i) { return 15.0f * sin(t * XM_2PI + z*8); };
+        colorFunc = NULL;
+        break;
+
+    case 4: // Quick move to back
+        for (auto& l : lightsScene) {
+            l.color = { 1.0f, 1.0f, 1.0f };
+            l.direction = { -1.0f, -1.0f, 0.0f };
+            l.isEnabled = true;
+        }
+        lightEffectPeriod = 8;
+        angVFunc = [](float z, float t, int i) { return 75.0f * (t>1.0/6 && t<5.0/6 ? 1 : sin(t*3*3.14159)); };
+        angHFunc = [](float z, float t, int i) { return 0.0f; };
+        colorFunc = NULL;
+        break;
+
+    case 5: // BLink with random rot toward center
+        for (auto& l : lightsScene) {
+            l.color = { 1.0f, 1.0f, 1.0f };
+            l.direction = { -1.0f, -1.0f, 0.0f };
+            l.isEnabled = true;
+        }
+        lightEffectPeriod = 8;
+        angVFunc = [](float z, float t, int i) { return 15.0f; };
+        angHFunc = [](float z, float t, int i) { return (10.0f - 60.0f * sin(1.57f+ 8*floor(t*4))) * z/3; };
+        colorFunc = [](float z, float t, int i) {
+            if (((int)(t*8))%2 == 1) return XMFLOAT3{ 1.0f, 1.0f, 1.0f };
             else return XMFLOAT3{ 0.0f, 0.0f, 0.0f };
             };
         break;
